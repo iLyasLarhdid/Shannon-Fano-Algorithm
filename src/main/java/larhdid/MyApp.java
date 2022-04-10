c7dd73529e4367e1aeb89aed947b3acf783a61e0
ilyas <larhdidok@gmail.com>
2022-04-10 18:31:17 +0100

    deleting unused variables and old comments

--- src/main/java/larhdid/MyApp.java
+++ src/main/java/larhdid/MyApp.java
@@ -12,18 +12,14 @@ public class MyApp {
         Map<Integer, Double> charOldAsciiTempSolution = new TreeMap<>();
         Map<String, Integer> dictAscii = new TreeMap<>();
         String text;
-        MyCompressionV4Finished v4 = new MyCompressionV4Finished();// next version you give a text in the constructor and based on it we get the percentages
+        MyCompressionV4Finished v4 = new MyCompressionV4Finished();
 
-        //instead of writing down the possibilities, we should generate them from a text
         System.out.println("Enter the text : ");
         text = s.nextLine();
         for(int i = 0; i<text.length(); i++){
             String key = String.valueOf(text.charAt(i));
             charOldAscii.put(key,charOldAscii.getOrDefault(key,0.)+1);
         }
-//        charOldAscii.forEach((key,value)->{
-//            charOldAscii.put(key,charOldAscii.get(key)/charOldAscii.size());
-//        });
 
         AtomicInteger counter = new AtomicInteger();
         charOldAscii.forEach((key,value)->{
@@ -33,7 +29,6 @@ public class MyApp {
         });
 
         System.out.println("Dictionary table"+dictAscii);
-        //v4.compress(charOldAscii);
         v4.compress(charOldAsciiTempSolution);
         System.out.println(v4.toString());
     }
--- src/main/java/larhdid/MyCompressionV4Finished.java
+++ src/main/java/larhdid/MyCompressionV4Finished.java
@@ -7,18 +7,14 @@ import java.util.concurrent.atomic.AtomicReference;
 public class MyCompressionV4Finished {
     Map<Integer, String> charNewAscii = new TreeMap<>();//map to store the new ascii code for the characters
 
-    //returns the dictionary
     public void compress(Map<Integer, Double> charOldAscii){
         List<Double> tab_results = new ArrayList<>();//results of each iteration ( the subtraction of numbers to get the Splitting point)
-        List<Integer> temp = new ArrayList<>();//a temporary list to store the ascii while calculating;
         AtomicInteger border = new AtomicInteger(1);//Splitting point its one by default
         //i am using atomic because i am changing a variable inside collection.foreach
         charOldAscii.forEach((key, value)->{
             int curIndex = key;
             AtomicReference<Double> sum1 = new AtomicReference<>((double) 0);
             AtomicReference<Double> sum2 = new AtomicReference<>((double) 0);
-            //break doesn't work in the foreach ( i should change the code to use takeWhile )
-            //charOldAscii.keySet().stream().takeWhile(condition).foreach(...)
             if(border.get()>1)
                 return;
             charOldAscii.forEach((key2, value2)->{
@@ -40,10 +36,6 @@ public class MyCompressionV4Finished {
                 }
             }
             tab_results.add(key,result_temp>0?result_temp:-1*result_temp);
-            //tab_results only has indexes : 0 and 1 at the moment
-            //the condition is true and you're asking it to give you the value of index
-            // curIndex-1 = 3-1 = 2
-            // !important : you should check if the charOldAscii's size is == 2, we give the first 0 and the second 1
             if(tab_results.size()>1 && tab_results.get(key-1)<tab_results.get(key)){
                 border.set(key);
             }
@@ -56,12 +48,9 @@ public class MyCompressionV4Finished {
         charOldAscii.forEach((key, value)->{
             int curIndex = key;
             if(curIndex < finalBorder){
-                //System.out.println(0);
-                //collect 0 if you have more than 2 zeros you call compress
                 char0Ascii.put(key,charOldAscii.get(key));
                 charNewAscii.put(key,charNewAscii.getOrDefault(key,"")+"0");
             }else{
-                //System.out.println(1);
                 char1Ascii.put(key,charOldAscii.get(key));
                 charNewAscii.put(key,charNewAscii.getOrDefault(key,"")+"1");
             }
